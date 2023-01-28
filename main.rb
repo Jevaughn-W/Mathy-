4dc35d90e937c4956b17dd93eb50dd122a7a4372
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-01-28 14:52:43 -0500

    Update case to snake case

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require "./Player.rb"
 
 
+
 # Game start
 
 puts "-----Welcome to Mathy!-----\n"
@@ -13,11 +14,11 @@ puts "\nPlease enter player 2 name."
 player2_name = gets.chomp
 
 # Create player 1 & 2; player 1 goes first
-Player1 = Player.new(player1_name, 0, 3, true)
-Player2 = Player.new(player2_name, 0, 3, false)
+player_one = Player.new(player1_name, 0, 3, true)
+player_two = Player.new(player2_name, 0, 3, false)
 
 
 
 # Object check
-puts  "Player 1: #{Player1.name}"
-puts  "Player 2: #{Player2.name}"
+puts  "Player 1: #{player_one.name}"
+puts  "Player 2: #{player_two.name}"
