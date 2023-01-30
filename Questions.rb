b1a7fff13d6138b4a117a2d184c066a69a49ada9
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-01-29 21:45:37 -0500

    Remove bug which prevents new numbers from being generated

--- Questions.rb
+++ Questions.rb
@@ -3,15 +3,16 @@ class Questions
   attr_accessor :sum
 
   def self.generate
-    random = Random.new(Random.seed) # Used to numbers closer to true randomness
-    @num_one = random.rand(1...20)
-    @num_two = random.rand(1...20)
-    "What does #{@num_one} plus #{@num_two} equal?"
+    # random = Random.new(Random.seed) # Used to numbers closer to true randomness
+    @num_one = rand(1...20)
+    @num_two = rand(1...20)
+    @question = "What does #{@num_one} plus #{@num_two} equal?"
   end
 
   def self.answer
     @sum = @num_one + @num_two
   end
-end
 
+
+end
 
