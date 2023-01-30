f6fce7390fa3aec54009f93db64959ec9c925d96
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-01-29 19:59:39 -0500

    Update turn method to isolate player whose turn is true and implement functionality to update score and lives

---
+++ Game.rb
@@ -0,0 +1,40 @@
+class Game
+  
+
+  def initialize(player_one, player_two)
+    @@players = [ player_one, player_two] #using class variable as no objects will be created here
+  end
+  
+  
+  # Implement function to check turn based on the player whose turn is true
+  def self.turn
+    @@players.each do |player|
+      if player.turn == true
+        @@selected = player
+      else
+        @@nexturn = player
+      end
+      
+    end
+
+    # Print out the generated question with the player name and accept palayer answer
+
+    puts "\n#{@@selected.name}: #{Questions.generate}" 
+    player_answer = gets.chomp
+
+    if player_answer.to_i == Questions.answer
+      @@selected.score += 1
+      puts "\nYES! Your are correct."
+      puts "#{@@selected.name}: #{@@selected.lives}/3 vs #{@@nexturn.lives}: #{@@nexturn.lives}/3"
+    else 
+      @@selected.lives -= 1
+      puts "\nSeriously? No!"
+      puts "#{@@selected.name}: #{@@selected.lives}/3 vs #{@@nexturn.name}: #{@@nexturn.lives}/3"
+    end
+
+  end
+
+
+end
+
+# puts Game.players
