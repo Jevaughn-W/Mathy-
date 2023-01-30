e76f6eb430e555772849025bd313af17bd362b59
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-01-29 21:51:00 -0500

    Remove dead code

--- Questions.rb
+++ Questions.rb
@@ -3,7 +3,6 @@ class Questions
   attr_accessor :sum
 
   def self.generate
-    # random = Random.new(Random.seed) # Used to numbers closer to true randomness
     @num_one = rand(1...20)
     @num_two = rand(1...20)
     @question = "What does #{@num_one} plus #{@num_two} equal?"
